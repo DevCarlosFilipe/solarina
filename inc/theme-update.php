a0da85801a01fdf8db51e0e3d1dc5d1cacbf034b
Carlos Filipe <lordcfprogramador@gmail.com>
2026-03-30 21:22:51 -0300

    Adiciona Plugin Update Checker e ajusta atualizacao de tema

--- inc/theme-update.php
+++ inc/theme-update.php
@@ -1,10 +1,10 @@
 <?php
 
-require get_template_directory() . '/inc/theme-update-checker/theme-update-checker.php';
+require_once get_template_directory() . '/inc/theme-update-checker/theme-update-checker.php';
 
 $updateChecker = Puc_v4_Factory::buildUpdateChecker(
     'https://github.com/DevCarlosFilipe/solarina',
-    __FILE__,
+    get_template_directory() . '/style.css',
     'solarina'
 );
 
