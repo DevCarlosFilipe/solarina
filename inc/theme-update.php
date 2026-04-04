126e5f4ac5341d31f280b9048cf9948646dfc68b
Carlos Filipe <lordcfprogramador@gmail.com>
2026-04-03 22:53:33 -0300

    Update theme-update.php

--- inc/theme-update.php
+++ inc/theme-update.php
@@ -1,11 +1,15 @@
 <?php
 
-require_once get_template_directory() . '/inc/theme-update-checker/theme-update-checker.php';
+add_action('after_setup_theme', function () {
 
-$updateChecker = Puc_v4_Factory::buildUpdateChecker(
-    'https://github.com/DevCarlosFilipe/solarina',
-    get_template_directory() . '/style.css',
-    'solarina'
-);
+    require_once get_template_directory() . '/inc/theme-update-checker/theme-update-checker.php';
 
-$updateChecker->setBranch('main');
+    $updateChecker = Puc_v4_Factory::buildUpdateChecker(
+        'https://github.com/DevCarlosFilipe/solarina',
+        get_template_directory(),
+        'solarina'
+    );
+
+    $updateChecker->setBranch('main');
+
+});
