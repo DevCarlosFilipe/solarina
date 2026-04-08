6d279268ac8463e5e6a1899eaa34d2198e52415f
Carlos Filipe <lordcfprogramador@gmail.com>
2026-04-07 23:10:06 -0300

    Update theme-update.php

--- inc/theme-update.php
+++ inc/theme-update.php
@@ -2,6 +2,10 @@
 
 add_action('after_setup_theme', function () {
 
+    // CARREGA PARSEDOWN
+    require_once get_template_directory() . '/inc/parsedown/Parsedown.php';
+
+    // UPDATE CHECKER
     require_once get_template_directory() . '/inc/theme-update-checker/theme-update-checker.php';
 
     $updateChecker = Puc_v4_Factory::buildUpdateChecker(
