bdefa1bafd44003dee1ce5d813b14e25e61ce690
Carlos Filipe <lordcfprogramador@gmail.com>
2026-05-04 19:28:14 -0300

    Remove duplicação da seção "Lojas" no front-page.php

--- front-page.php
+++ front-page.php
@@ -4,9 +4,9 @@
 
     <?php get_template_part('template/sections/hero'); ?>
     <?php get_template_part('template/sections/featured', 'products'); ?>
+    <?php get_template_part('template/sections/lojas'); ?>
     <?php get_template_part('template/sections/info', 'section'); ?>
     <?php get_template_part('template/sections/categories'); ?>
-    <?php get_template_part('template/sections/lojas'); ?>
     <?php get_template_part('template/sections/follow'); ?>
 
 </main>
